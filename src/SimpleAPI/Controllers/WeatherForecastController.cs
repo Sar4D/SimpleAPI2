22ad7fdda7f0eddf4ed74903f3bc3d63d28ba228
Sar4D <100158491+Sar4D@users.noreply.github.com>
2022-04-11 13:44:34 -0400

    Add files via upload

---
+++ src/SimpleAPI/Controllers/WeatherForecastController.cs
@@ -0,0 +1,46 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace SimpleAPI.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class WeatherForecastController : ControllerBase
+{
+    [HttpGet]
+    public ActionResult<IEnumerable<string>> Get()
+    {
+       return new string[] {"Value1","Value2" };
+    }
+
+    [HttpGet("{id}")]
+    public ActionResult<string> Get(int id)
+    {
+       return "Joey";
+    }
+
+
+
+ //   private static readonly string[] Summaries = new[]
+ //   {
+ //       "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+  //  };
+
+  //  private readonly ILogger<WeatherForecastController> _logger;
+
+  //  public WeatherForecastController(ILogger<WeatherForecastController> logger)
+  //  {
+ //       _logger = logger;
+  //  }
+
+   // [HttpGet(Name = "GetWeatherForecast")]
+  //  public IEnumerable<WeatherForecast> Get()
+   // {
+   //     return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+   //     {
+   //         Date = DateTime.Now.AddDays(index),
+   //         TemperatureC = Random.Shared.Next(-20, 55),
+   //         Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+   //     })
+   //     .ToArray();
+  //  }
+}
